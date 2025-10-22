10fb7cd3055c3ce5d6fb70a07273a1d133ae63c4
VictorDoom022 <victordoom022@gmail.com>
2025-10-22 16:20:36 +0800

    feat: basic CRUD

---
+++ lib/edit_screen.dart
@@ -0,0 +1,173 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'models/note.dart';
+
+
+enum EditScreenMode {
+  VIEW,
+  ADD,
+  EDIT,
+}
+
+class EditScreen extends StatefulWidget {
+
+  final EditScreenMode mode;
+  final Note? note;
+
+  const EditScreen({super.key, required this.mode, this.note});
+
+  @override
+  State<EditScreen> createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+
+  bool isEditMode = false;
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController titleTextEditingController = TextEditingController();
+  TextEditingController contentTextEditingController = TextEditingController();
+
+  FirebaseFirestore database = FirebaseFirestore.instance;
+  User? loggedInUser = FirebaseAuth.instance.currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    switch(widget.mode) {
+      case EditScreenMode.VIEW:
+        isEditMode = false;
+        titleTextEditingController.text = widget.note?.title ?? '';
+        contentTextEditingController.text = widget.note?.content ?? '';
+        break;
+      case EditScreenMode.ADD:
+        isEditMode = true;
+        break;
+      case EditScreenMode.EDIT:
+        isEditMode = true;
+        titleTextEditingController.text = widget.note?.title ?? '';
+        contentTextEditingController.text = widget.note?.content ?? '';
+        break;
+    }
+  }
+
+  Future<void> saveNote(Note note) async {
+    if(loggedInUser == null) return;
+    try {
+      DocumentReference<Map<String, dynamic>> result = await database.collection('notes')
+          .doc(loggedInUser!.uid)
+          .collection('userNotes')
+          .add(note.toJson());
+
+      Navigator.pop(context); // Task B9
+    } catch(e) {
+      print(e);
+      if(!kDebugMode) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.toString()))
+      );
+    }
+  }
+
+  Future<void> updateNote(Note note) async {
+    if (loggedInUser == null || note.id == null) return;
+
+    try {
+      await database
+          .collection('notes')
+          .doc(loggedInUser!.uid)
+          .collection('userNotes')
+          .doc(note.id)
+          .update(note.toJson());
+
+      Navigator.pop(context); // Task B9
+    } catch (e) {
+      print(e);
+      if(!kDebugMode) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to update note: $e')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Text(renderAppBarTitle()),
+        actions: [
+          widget.mode != EditScreenMode.VIEW ? IconButton(
+            icon: const Icon(Icons.check_circle),
+            onPressed: () async {
+              Note note = Note(
+                id: widget.note?.id,
+                title: titleTextEditingController.text,
+                content: contentTextEditingController.text,
+              );
+              if(widget.note == null) {
+                await saveNote(note);
+              } else {
+                await updateNote(note);
+              }
+            },
+          ) : const SizedBox.shrink(),
+          IconButton(
+            icon: const Icon(Icons.cancel),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    TextField(
+                      enabled: isEditMode,
+                      controller: titleTextEditingController,
+                      decoration: const InputDecoration(
+                        hintText: 'Title',
+                      ),
+                    ),
+                    TextField(
+                      enabled: isEditMode,
+                      minLines: 3,
+                      maxLines: 10,
+                      controller: contentTextEditingController,
+                      decoration: const InputDecoration(
+                          hintText: 'Content',
+                          border: InputBorder.none
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  String renderAppBarTitle() {
+    switch(widget.mode) {
+      case EditScreenMode.VIEW:
+        return 'View Note';
+      case EditScreenMode.ADD:
+        return 'Add new Note';
+      case EditScreenMode.EDIT:
+        return 'Edit Note';
+    }
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'edit_screen.dart';
+import 'models/note.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,15 +14,238 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  FirebaseFirestore database = FirebaseFirestore.instance;
+  User? loggedInUser = FirebaseAuth.instance.currentUser;
+
+  bool isExpanded = true;
+  String? focusedNoteID;
+  List<Note> noteList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      loadNotes();
+    });
+  }
+
+  Future<void> loadNotes() async {
+    if(loggedInUser == null) return;
+    QuerySnapshot<Map<String, dynamic>> result = await database.collection('notes')
+        .doc(loggedInUser!.uid)
+        .collection('userNotes')
+        .get();
+
+    List<Note> notes = result.docs.map((e) {
+      Note note = Note.fromJson(e.data());
+      note.id = e.id;
+      return note;
+    }).toList();
+
+    setState(() {
+      noteList = notes;
+    });
+  }
+
+  Future<void> deleteNote(String id) async {
+    if(loggedInUser == null) return;
+    try {
+      await database.collection('notes')
+          .doc(loggedInUser!.uid)
+          .collection('userNotes')
+          .doc(id)
+          .delete();
+      await loadNotes();
+
+      if(!kDebugMode) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Note deleted')),
+      );
+    } catch(e) {
+      print(e);
+      if(!kDebugMode) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('My Notes'),
         automaticallyImplyLeading: false,
-        title: Text(
-          'Bloc Test'
+        actions: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.only(right: 16),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.3),
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Text(
+                noteList.length.toString(),
+                style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: noteList.length,
+              itemBuilder: (context, index) {
+                bool isLastItem = index == noteList.length - 1;
+
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: isExpanded ? 8 : 18,
+                          horizontal: isExpanded ? 8 : 16
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => EditScreen(
+                                note: noteList[index],
+                                mode: EditScreenMode.VIEW,
+                              ),
+                            ),
+                          );
+                        },
+                        onLongPress: () {
+                          if(focusedNoteID == noteList[index].id) {
+                            focusedNoteID = null;
+                          } else {
+                            focusedNoteID = noteList[index].id;
+                          }
+                          setState(() { });
+                        },
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    noteList[index].title ?? 'No title',
+                                    style: const TextStyle(
+                                        fontSize: 22
+                                    ),
+                                  ),
+                                  isExpanded ? Text(
+                                    noteList[index].content ?? 'No content',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ) : const SizedBox.shrink(),
+                                ],
+                              ),
+                            ),
+                            focusedNoteID == noteList[index].id ? Row(
+                              children: [
+                                const SizedBox(width: 10),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.edit,
+                                    color: Theme.of(context).colorScheme.primary,
+                                  ),
+                                  onPressed: () async {
+                                    await Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) => EditScreen(
+                                          note: noteList[index],
+                                          mode: EditScreenMode.EDIT,
+                                        ),
+                                      ),
+                                    );
+                                    await loadNotes();
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.delete,
+                                    color: Theme.of(context).colorScheme.primary,
+                                  ),
+                                  onPressed: () async {
+                                    if(noteList[index].id == null) return;
+                                    await deleteNote(noteList[index].id!);
+                                  },
+                                )
+                              ],
+                            ) : const SizedBox.shrink(),
+                          ],
+                        ),
+                      ),
+                    ),
+                    isLastItem ? const SizedBox.shrink() : const Divider(color: Colors.grey,),
+                  ],
+                );
+              },
+            )
+          ],
         ),
       ),
+      floatingActionButton: buildFloatingActionButtonSecion(),
+    );
+  }
+
+  Widget buildFloatingActionButtonSecion() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TextButton(
+          onPressed: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+          child: Icon(
+            isExpanded ? Icons.unfold_less : Icons.menu,
+            color: Colors.white,
+          ),
+          style: ButtonStyle(
+            padding: const MaterialStatePropertyAll(EdgeInsets.all(16)),
+            shape: const MaterialStatePropertyAll(CircleBorder()),
+            backgroundColor: MaterialStatePropertyAll(Theme.of(context).colorScheme.primary),
+          ),
+        ),
+        const SizedBox(width: 10),
+        TextButton(
+          onPressed: () async {
+            await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const EditScreen(mode: EditScreenMode.ADD),
+                )
+            );
+            await loadNotes();
+          },
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          style: ButtonStyle(
+            padding: const MaterialStatePropertyAll(EdgeInsets.all(16)),
+            shape: const MaterialStatePropertyAll(CircleBorder()),
+            backgroundColor: MaterialStatePropertyAll(Theme.of(context).colorScheme.primary),
+          ),
+        ),
+      ],
     );
   }
 }
